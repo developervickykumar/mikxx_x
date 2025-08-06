89ecc2eef3cdc617cbcda91c9171466c5526523f
developervickykumar <developer_vicky@mikxx.com>
2025-08-06 11:09:06 +0530

    this api key change

--- config/openai.php
+++ config/openai.php
@@ -1,8 +1,7 @@
 <?php
 
 return [
-  'api_key'=>'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=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',
-    //'api_key' => env('OPENAI_API_KEY'), // moved to .env
+    'api_key' => env('OPENAI_API_KEY'), // moved to .env
     'organization' => env('OPENAI_ORGANIZATION'), // Optional
     'base_uri' => env('OPENAI_BASE_URI', 'https://api.openai.com/v1'),
     'http_client_handler' => null,
